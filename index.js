af183a1ebe525b7a09dff9d45429c4df2bbff9ad
groodking <62711371+groodking@users.noreply.github.com>
2020-04-05 01:10:48 +0530

    dice game

---
+++ index.js
@@ -0,0 +1,65 @@
+var randomnumber1 = Math.floor(Math.random()*6+1);
+var randomnumber2 = Math.floor(Math.random()*6+1);
+
+if(randomnumber1==1)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}
+else if (randomnumber1==2)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}
+else if (randomnumber1==3)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}
+else if (randomnumber1==4)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}
+else if (randomnumber1==5)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice5.png");
+}
+else if (randomnumber1==6)
+{
+  document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
+//For second dice
+if(randomnumber2==1)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}
+else if (randomnumber2==2)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}
+else if (randomnumber2==3)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}
+else if (randomnumber2==4)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}
+else if (randomnumber2==5)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+else if (randomnumber2==6)
+{
+  document.querySelector(".img2").setAttribute("src","images/dice6.png");
+}
+// results
+if(randomnumber1==randomnumber2)
+{
+      document.querySelector("h1").innerHTML="Draw!";
+}
+else if (randomnumber1>randomnumber2)
+{
+      document.querySelector("h1").innerHTML="Player 1 Wins! 🚩";
+}
+else
+{
+  document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
